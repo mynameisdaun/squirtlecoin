9f443f9372d6c37ec73955b69f468ff071393140
mynameisdaun <daun9870.jung@gmail.com>
2022-06-16 00:57:17 +0900

    move package for blockchain

--- main.go
+++ main.go
@@ -1,42 +1,8 @@
 package main
 
-import (
-	"crypto/sha256"
-	"fmt"
-)
-
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	newBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(newBlock.data+newBlock.prevHash)))
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockchain) listBlocks() {
-	for idx, block := range b.blocks {
-		fmt.Printf("index: %d, Data : %s, Curr Hash: %s, Prev Hash: %s\n", idx, block.data, block.hash, block.prevHash)
-	}
-}
+import "github.com/mynameisdaun/squirtlecoin/blockchain"
 
 func main() {
-	chain := blockchain{}
-	chain.addBlock("first")
-	chain.addBlock("second")
-	chain.listBlocks()
+	chain := blockchain.GetBlockchain()
+
 }
